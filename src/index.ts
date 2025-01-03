6f8afed8fefb128c062dad5a2240476ca44ec6ba
stas_fr <sfworking@yandex.ru>
2025-01-03 12:59:04 +0300

    types: add types imports from express

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
+import type { Request, Response } from 'express';
 import * as fs from 'fs';
 
 import { uploadObject } from './s3.js';
@@ -10,17 +11,17 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/', function (request, response) {
+app.get('/', function (request: Request, response: Response) {
   response.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/heartbeat', function (request, response) {
+app.get('/heartbeat', function (request: Request, response: Response) {
   response.json({
     message: 'Hello World!',
   });
 });
 
-app.post('/upload', async function (request, response) {
+app.post('/upload', async function (request: Request, response: Response) {
   try {
     const file = fs.readFileSync('./test/file.pdf');
 
