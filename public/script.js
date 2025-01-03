7be878104dc893922dc61b3c360311df8054148f
stas_fr <sfworking@yandex.ru>
2025-01-03 12:23:18 +0300

    feat: update frontend

--- public/script.js
+++ public/script.js
@@ -1,1 +1,18 @@
-console.log('test');
+const fileInput = document.querySelector('#fileInput');
+fileInput.addEventListener('change', sendFile);
+
+async function sendFile(event) {
+  const currentFile = event.target.files[0];
+
+  const formData = new FormData();
+  formData.append('file', currentFile);
+
+  const response = await fetch('/upload', {
+    body: {
+      test: 'test',
+    },
+    method: 'POST',
+  });
+
+  console.log(response);
+}
